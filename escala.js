5cb5aba74158470449ec7ce9b43962717763e616
FranciscoArenas <francisco_arenas@live.cl>
2021-01-25 08:55:30 -0300

    test

---
+++ escala.js
@@ -0,0 +1,4 @@
+function escala(a, b) {
+    return a + b;
+  }
+  module.exports = escala;
---
+++ escala.test.js
@@ -0,0 +1,5 @@
+const escala = require('./escala');
+
+test('adds 1 + 2 to equal 3', () => {
+  expect(escala(1, 2)).toBe(3);
+});
