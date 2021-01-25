04f9ec5502c6f366ae3e224a87e8b58bb2e9c7c8
FranciscoArenas <francisco_arenas@live.cl>
2021-01-25 09:07:23 -0300

    Revert "test"
    This reverts commit 5cb5aba74158470449ec7ce9b43962717763e616.

--- escala.js
+++
@@ -1,4 +0,0 @@
-function escala(a, b) {
-    return a + b;
-  }
-  module.exports = escala;
--- escala.test.js
+++
@@ -1,5 +0,0 @@
-const escala = require('./escala');
-
-test('adds 1 + 2 to equal 3', () => {
-  expect(escala(1, 2)).toBe(3);
-});
